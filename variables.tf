a9953e3f6f4c1824d23e0e360a867a789ba2e652
centos <centos@localhost.localdomain>
2020-05-10 04:03:04 +0300

    remove unused variables

--- variables.tf
+++ variables.tf
@@ -19,11 +19,3 @@ variable "dns_key_algorithm" {
 variable "dns_key_secret" {
   type = string
 }
-
-variable "cloudflare_zone_id_teamvinted_com" {
-  type = string
-  default = "b0da52d77c310d5a747b05da9c9b9515"
-}
-
-
-
